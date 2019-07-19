0cc51d3e7e603f6c0ecd8bec8b23029958d0a083
Stefan Obst <stefanobst@Stefans-MacBook-Pro-2.local>
2019-07-19 15:17:19 +0200

    ShopPage and Collection-Preview created without any styling

--- src/App.js
+++ src/App.js
@@ -2,22 +2,17 @@ import React from 'react';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shop/shoppage.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route exact path='/shop/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ ShopPage } />
       </Switch>
     </div>
   );
 }
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
 
 export default App;
---
+++ src/pages/shop/shoppage.component.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import SHOP_DATA from './shop_data'
+
+class ShopPage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+           collections: SHOP_DATA
+        };
+
+    }
+
+    render() {
+        return (
+
+            <div>
+
+                {this.state.collections.map(({id, ...otherProperties}) => (
+                    <CollectionPreview key={id} {...otherProperties}/>
+                ))}
+            </div>
+
+
+        )
+    }
+
+
+
+}
+
+export default ShopPage;
